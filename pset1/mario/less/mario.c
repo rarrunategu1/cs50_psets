831286e09b75ebbd9d36b08b5ebb644f6e223e6f
Rose Parker <parker.rose@ymail.com>
2018-11-22 23:19:03 +0000

    corrof the first for loop since it starts at 0

--- pset1/mario/less/mario.c
+++ pset1/mario/less/mario.c
@@ -1,11 +1,14 @@
+
 #include <cs50.h>
 #include <stdio.h>
 
+//used for building the ladder
 #define BLOCK "#"
-#define SPACE "."
+#define SPACE " "
 
 int main(void)
 {
+    //validation checks to check if user input for height is between 0 and 23 or it keeps asking
     int height;
     do
     {
@@ -13,16 +16,17 @@ int main(void)
     }
     while (height < 0 || height > 23);
 
-
+    //first for loop iterates through height input to print as many lines as the input
     int i;
     for (i = 0; i < height; i++)
     {
-
-        for(int k = 0; k<i; k++)
+        //2nd for loop: j = height plus 1 to take in entire height printed and prints one less space as long as j is less than i+2 which would equl entire height of the ladder printed
+        for (int j = height + 1; j > i + 2; j--)
         {
             printf(SPACE);
+        //3rd for loop: k = 0 to build the ladder with the hashes.  adds one has as long as k is less than i plus two(height of ladder printed)
         }
-        for(int j = 0; j<i+2; j++)
+        for (int k = 0; k < i + 2; k++)
         {
             printf(BLOCK);
         }
