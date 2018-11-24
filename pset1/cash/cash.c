ecbf8f04fd546233b743d3419bda27a52eb8e42b
Rose Parker <parker.rose@ymail.com>
2018-11-24 00:42:32 +0000

    float input is converted to cents by multiplying it by 100

--- pset1/cash/cash.c
+++ pset1/cash/cash.c
@@ -1,13 +1,24 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <math.h>
 
 int main(void)
 {
-int change;
-do
-{
- change = get_float("Change owed: ");
+    float change;
+    // float coin [4] = {0.25, 0.10, 0.05, 0.01};
+    // int totalCoins = 0;
+    do
+        {
+        change = get_float("Change owed: ");
 
-}
-while(change < 0);
+        }
+    while(change < 0);
+    change = change * 100;
+
+
+    printf("%f\n", change);
+
+
+    //printf("%.2f\n", change * 100);
+
 }
