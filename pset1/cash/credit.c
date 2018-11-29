f04c96ded52d07d98e12bcedd530017e93ac67e2
Rose Parker <parker.rose@ymail.com>
2018-11-29 19:33:25 +0000

    refactored to remove repetitive code

--- pset1/cash/credit.c
+++ pset1/cash/credit.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <string.h>
 
 #define MAX16 5599999999999999
 #define MAXV16 4999999999999999
@@ -24,6 +25,24 @@ int main(void)
     if (ccnum >= MIN16 && ccnum <= MAX16)
     {
         length = 16;
+    }
+
+    else if (ccnum >= MIN15 && ccnum <= MAX15)
+    {
+        length = 15;
+    }
+
+    else if (ccnum >= MIN13 && ccnum <= MAX13)
+    {
+        length = 13;
+    }
+    else
+    {
+        printf("INVALID\n");
+        return 0;
+    }
+
+    if (length <= 16)
         for (; length > 0; length--)
         {
             //gets each digit in the card number
@@ -41,71 +60,23 @@ int main(void)
                 secondNumber += number;
             }
         }
-        firstTwo = firstNumber + secondNumber;
 
-        //checks if the card is a master or visa based on first digits
-        if (firstNumber == 4)
-        {
-            printf("VISA\n");
-        }
+    firstTwo = firstNumber + secondNumber;
 
-        else if (firstNumber == 5 && firstTwo >= 6 && firstTwo <= 10)
-        {
-            printf("MASTERCARD");
-        }
-
-    }
-
-    else if (ccnum >= MIN15 && ccnum <= MAX15)
+    //checks if the card is a master or visa based on first digits
+    if (firstNumber == 4)
     {
-        length = 15;
-        for (; length > 0; length--)
-        {
-            int number = ccnum % 10;
-            ccnum = ccnum / 10;
-
-            if (length == 1)
-            {
-                firstNumber +=  number;
-            }
-            if (length == 2)
-            {
-                secondNumber += number;
-            }
-        }
-        firstTwo = firstNumber + secondNumber;
-
-        if (firstNumber == 3 && (firstTwo == 7 || firstTwo == 10))
-        {
-            printf("AMEX\n");
-        }
+        printf("VISA\n");
     }
-
-    else if (ccnum >= MIN13 && ccnum <= MAX13)
+    else if (firstNumber == 5 && firstTwo >= 6 && firstTwo <= 10)
     {
-        length = 13;
-        for (; length > 0; length--)
-        {
-            int number = ccnum % 10;
-            ccnum = ccnum / 10;
-
-            if (length == 1)
-            {
-                firstNumber +=  number;
-            }
-
-        }
-        if (firstNumber == 4)
-        {
-
-            printf("VISA\n");
-        }
+        printf("MASTERCARD\n");
     }
-    else
+    else if (firstNumber == 3 && (firstTwo == 7 || firstTwo == 10))
     {
-        printf("INVALID\n");
-        return 0;
+        printf("AMEX\n");
     }
+
 
     int timesTwo = 0;
     int noTimes = 0;
