d069eca269dcd233081d59c1714ea763f325ff04
Rose Parker <parker.rose@ymail.com>
2018-12-01 03:56:59 +0000

    used modifier to help in running luhns algorithm through even and odd numbered ccnums.  Also  added the else print Invalid after checking with Luhns algorithm

--- pset1/cash/credit.c
+++ pset1/cash/credit.c
@@ -15,11 +15,7 @@ int main(void)
 {
     long long ccnum = get_long_long("Number: ");
     int length = 0;
-    printf("%lld\n", ccnum);
-
-    int firstNumber = 0;
-    int secondNumber = 0;
-    int firstTwo = 0;
+    //printf("%lld\n", ccnum);
     int modifier = 0;
 
     if (ccnum >= MIN16 && ccnum <= MAX16)
@@ -27,12 +23,10 @@ int main(void)
         length = 16;
         modifier = 1;
     }
-
     else if (ccnum >= MIN15 && ccnum <= MAX15)
     {
         length = 15;
     }
-
     else if (ccnum >= MIN13 && ccnum <= MAX13)
     {
         length = 13;
@@ -43,6 +37,9 @@ int main(void)
         return 0;
     }
 
+    int firstNumber = 0;
+    int secondNumber = 0;
+    int firstTwo = 0;
     int timesTwo = 0;
     int noTimes = 0;
     int final = 0;
@@ -51,12 +48,31 @@ int main(void)
     {
         //gets each digit in the card number
         int number = ccnum % 10;
+        //printf("length = %i\tnumber: %i\n", length, number);
+
+        //This section is to check the first and last digit of each card.
+        //this if takes the number at length 1 and assigns the number adds then number to firstNumber
+        if (length == 1)
+        {
+            firstNumber =  number;
+            //printf("firstNumber: %i\n", firstNumber);
+        }
+        //takes the number at length 2 and assigns the number adds then number to secondNumber
+        else if (length == 2)
+        {
+            secondNumber = number;
+            //printf("secondNumber: %i\n", secondNumber);
+        }
         ccnum = ccnum / 10;
+        //printf("ccnum: %lld\n", ccnum);
+
+
 
         //grabs every even number length digit starting at end of card - Lughns algorithm in the if else statement.  final tally at final int lower in the code
         if ((length + modifier) % 2 == 0)
         {
             int temp = number * 2;
+            //printf("number: %i x 2\t = temp: %i\n", number, temp);
 
             timesTwo += temp % 10 + temp / 10;
         }
@@ -64,47 +80,38 @@ int main(void)
         {
             noTimes += number;
         }
-
-        //This section is to check the first and last digit of each card.
-        //this if takes the number at length 1 and assigns the number adds then number to firstNumber
-        if (length == 1)
-        {
-            firstNumber +=  number;
-        }
-        //takes the number at length 2 and assigns the number adds then number to secondNumber
-        if (length == 2)
-        {
-            secondNumber += number;
-        }
     }
+
     //printf("timesTwo: %i\tnoTimes: %i\n", timesTwo, noTimes);
     final = noTimes + timesTwo;
     firstTwo = firstNumber + secondNumber;
+    //printf("final: %i\n", final);
 
-    //checks if the card is a master or visa based on first digits
-    if (firstNumber == 4)
-    {
-        printf("VISA\n");
-    }
-    else if (firstNumber == 5 && firstTwo >= 6 && firstTwo <= 10)
-    {
-        printf("MASTERCARD\n");
-    }
-    else if (firstNumber == 3 && (firstTwo == 7 || firstTwo == 10))
-    {
-        printf("AMEX\n");
-    }
 
     //if last digit of the final answer is 0 then the card is valid
     if (final % 10 == 0)
     {
-        printf("VALID\n");
+        //checks if the card is a master or visa based on first digits
+        if (firstNumber == 4)
+        {
+            printf("VISA\n");
+        }
+        else if (firstNumber == 5 && firstTwo >= 6 && firstTwo <= 10)
+        {
+            printf("MASTERCARD\n");
+        }
+        else if (firstNumber == 3 && (firstTwo == 7 || firstTwo == 10))
+        {
+            printf("AMEX\n");
+        }
     }
     else
     {
         printf("INVALID\n");
     }
 
-    //prints final number to see why the number is invalid since the sum doesn't end in 0
-    printf("final%i\n", final);
+        //prints final number to see why the number is invalid since the sum doesn't end in 0
+        //printf("final%i\n", final);
+
 }
+
