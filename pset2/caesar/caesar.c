af69da09cc7cb32ca9dbf6e059a3b95c994417a7
Rose Parker <parker.rose@ymail.com>
2018-12-03 03:44:25 +0000

    Styled and added more comments

--- pset2/caesar/caesar.c
+++ pset2/caesar/caesar.c
@@ -4,48 +4,53 @@
 #include <string.h>
 #include <ctype.h>
 
-int main(int argk, string argv[])
+int main(int argk, string argkey[])
 {
     int key = 0;
-    int alphaIndex = 0;
 
     if (argk == 2)
     {
         //turns key input to an integer from a string
-        key = atoi(argv[1]);
+        key = atoi(argkey[1]);
 
 
-    //gets user plaintext input
-    string message = get_string("plaintext: ");
-    //printf("%s\n", message);
-    printf("cyphertext: ");
+        //gets user plaintext input
+        string message = get_string("plaintext: ");
+        //printf("%s\n", message);
+        printf("cyphertext: ");
 
-    for(int i = 0; i < strlen(message); i++)
-    {
-        //printf("%c\n", message[i]);
-
-        if (isalpha(message[i]))
+        for (int i = 0; i < strlen(message); i++)
         {
-            if (isupper(message[i]))
+            //printf("%c\n", message[i]);
+
+            //if alpha is true will return the character based on if uppercase or lowercase
+            if (isalpha(message[i]))
             {
-                printf("%c", (((message[i] - 'A') + key) % 26) + 'A');
+                if (isupper(message[i]))
+                {
+                    //takes character,converts it to a 0-26 alpha index, adds the key,
+                    //modulus allows it to go from z to a or Z to A and we add the Ascii
+                    //'A' a the end to get the ascii character back
+                    printf("%c", (((message[i] - 'A') + key) % 26) + 'A');
+                }
+                else if (islower(message[i]))
+                {
+                    printf("%c", (((message[i] - 'a') + key) % 26) + 'a');
+                }
             }
-            else if (islower(message[i]))
-            {
-                printf("%c", (((message[i] - 'a') + key) % 26) + 'a');
-            }
-        }
-        else if(isalpha(message[i]) == 0)
+            //if there is a character that is not alpha returning 0 then it will print the character as is
+            else if (isalpha(message[i]) == 0)
             {
                 printf("%c", message[i]);
             }
         }
     }
-else
-{
-    printf("USAGE: arguments key \n");
-    return 1;
-}
+    else
+    {
+        //prints error message if an argument if there are more or less than 2 arguments
+        printf("USAGE: arguments key \n");
+        return 1;
+    }
     printf("\n");
     return 0;
 }
