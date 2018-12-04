8836e189d749078bcb7c522a32cebfb17ea23265
Rose Parker <parker.rose@ymail.com>
2018-12-04 01:18:47 +0000

    for loop goes through alpha character and returns the position and letter of the argument keyword

--- pset2/vigenere/vigenere.c
+++ pset2/vigenere/vigenere.c
@@ -1,16 +1,63 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <ctype.h>
 
-int main(int argv, string argk[])
+int main(int argk, string argkey[])
 {
-    if (argv == 2 && isalpha(argk[1]))
+    string keyword = argkey[1];
+    int indexTotals;
+    char letter;
+
+    for (int i = 0; i < strlen(keyword); i++)
     {
-        printf("argument: %s\n", argk[1]);
+        indexTotals = i;
+        letter = keyword[i];
+        printf("position: %i\t letter: %c\t numberofplaces: %i\n", i, keyword[i], indexTotals);
+    }
+
+    if (argk == 2)
+    {
+        //gets user plaintext input
+        string message = get_string("plaintext: ");
+        //printf("%s\n", message);
+        printf("ciphertext: ");
+
+    //     for (int i = 0; i < strlen(message); i++)
+    //     {
+    //         //printf("%c\n", message[i]);
+
+    //         //if alpha is true will return the character based on if uppercase or lowercase
+    //         if (isalpha(message[i]))
+    //         {
+    //             if (isupper(message[i]))
+    //             {
+    //                 //takes character,converts it to a 0-26 alpha index, adds the key,
+    //                 //modulus allows it to go from z to a or Z to A and we add the Ascii
+    //                 //'A' a the end to get the ascii character back
+    //                 printf("%c", (((message[i] - 'A') + key) % ) + 'A');
+    //             }
+    //             else if (islower(message[i]))
+    //             {
+    //                 printf("%c", (((message[i] - 'a') + key) % i) + 'a');
+    //             }
+    //         }
+    //         //if there is a character that is not alpha returning 0 then it will print the character as is
+    //         else if (isalpha(message[i]) == 0)
+    //         {
+    //             printf("%c", message[i]);
+    //         }
+    //     }
     }
     else
     {
-        printf("USAGE: argument keyword\n");
+        //prints error message if an argument if there are more or less than 2 arguments
+        printf("USAGE: arguments key \n");
         return 1;
     }
+    printf("\n");
+    return 0;
 }
+
+
