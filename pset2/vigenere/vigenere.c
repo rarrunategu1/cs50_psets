485b4fe339992e78b993059fad83f4c5ab95a949
Rose Parker <parker.rose@ymail.com>
2018-12-06 01:43:40 +0000

    vigenere is providing non alphabetical characters in the cipher.  the rest of the code works

--- pset2/vigenere/vigenere.c
+++ pset2/vigenere/vigenere.c
@@ -6,69 +6,67 @@
 
 int main(int argk, string argkey[])
 {
-    string keyword = argkey[1];
-    int index = 0;
+    string keyWord = argkey[1];
+    int keyLength = 0;
     int letter = 0;
+    string message;
 
-    for (int i = 0; i < strlen(keyword); i++)
+    //if no keyword is given then an error returns
+    if (argk != 2)
     {
-        index = i;
-        letter = keyword[i];
+        printf("USAGE: arguments key \n");
+        return 1;
+    }
 
-        if (isalpha(keyword[i]))
+    //for loop stores the length of the keyword and checks is the keyword is alpha, if not alpha error returns
+    for (int i = 0, j = strlen(keyWord); i < j; i++)
+    {
+        keyLength = i;
+
+        if (isalpha(keyWord[i]))
         {
             continue;
         }
         else
         {
-            printf("USAGE: %s k\n", argkey[0]);
+            printf("USAGE: %s Alpha Keyword Only\n", argkey[0]);
             return 1;
         }
     }
-    //printf("position: %i\t letter: %c\n", index, letter);
+
+    //if there are two arguments we ask the user for a message
     if (argk == 2)
     {
-        //gets user plaintext input
-        string message = get_string("plaintext: ");
-        //printf("%s\n", message);
+        message = get_string("plaintext: ");
         printf("ciphertext: ");
-
-        for (int i = 0, j = 0, length = strlen(message); i < length; i++, j++)
-        {
-
-            //if alpha is true will return the character based on if uppercase or lowercase
-            if (isalpha(message[j]))
-            {
-
-                    if (isupper(message[j]))
-                    {
-                        printf("%c", (message[j] - 'A') + 'A');
-                    }
-                    else if (islower(message[j]))
-                    {
-                        printf("%c", (message[j] - 'a') + 'a');
-                    }
-
-                    //printf("plaintext: %c\t cypherletter: %c\t position: %i\n",  message[j], letter, index);
-
-                }
-
-            //if there is a character that is not alpha returning 0 then it will print the character as is
-            else if (isalpha(message[j]) == 0)
-            {
-                //printf("%c", message[j]);
-            }
-}
-
     }
-    else
+
+    //make the keylength it's actual length and not the ending position
+    int keyLengthTotal = keyLength + 1;
+
+
+    for (int i = 0, j = 0, length = strlen(message); i < length; i++, j++)
     {
-        //prints error message if an argument if there are more or less than 2 arguments
-        printf("USAGE: %s k\n", argkey[0]);
-        return 1;
+        //if alpha is true will return the character based on if uppercase or lowercase
+        if (isalpha(message[i]))
+        {
+            //calculates the ascii of each key position and wraps it around when it gets to the end.
+            char keyAscii = (isupper(keyWord[j % keyLengthTotal])) ? 'A' : 'a';
+
+            //calculates the ascii of each message position
+            char messageAscii = (isupper(message[i])) ? 'A' : 'a';
+
+            //gets index value every letter of the message + the keyword index value with wrap around
+            // minus the ascii of each key position, then we wrap around the alphabet if need be and turn it back into ascii code
+            printf("%c", ((message[i] - messageAscii) + (keyWord[j % keyLengthTotal] - keyAscii) % 26) + messageAscii);
+        }
+        //if there is a character that is not alpha then it will print the character as is
+        else if (isalpha(message[i]) == 0)
+        {
+            printf("%c", message[i]);
+        }
     }
+
     printf("\n");
     return 0;
- }
-
-
+}
