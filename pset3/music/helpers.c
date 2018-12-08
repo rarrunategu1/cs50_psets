440407271a8f5bda8b2a09bdc5396be33028e725
Rose Parker <parker.rose@ymail.com>
2018-12-08 18:48:34 +0000

    Music files for pset 3.  helper.c now contains the is_rest function which checks if the string is a rest or not

---
+++ pset3/music/helpers.c
@@ -0,0 +1,31 @@
+// Helper functions for music
+
+#include <cs50.h>
+#include <string.h>
+
+#include "helpers.h"
+
+// Converts a fraction formatted as X/Y to eighths
+int duration(string fraction)
+{
+    // TODO
+}
+
+// Calculates frequency (in Hz) of a note
+int frequency(string note)
+{
+    // TODO
+}
+
+// Determines whether a string represents a rest
+bool is_rest(string s)
+{
+    if (strlen(s) == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
