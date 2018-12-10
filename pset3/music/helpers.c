11b084c74d58230d8ed48f92daa340f75b098977
Rose Parker <parker.rose@ymail.com>
2018-12-10 03:59:43 +0000

    added frequency which outputs the correct sound.  In running the tests there is an error that having to do with null value in strtok on synthesize file.  Working on trying to figure out how to let it read the last character of the fraction which is the denominator

--- pset3/music/helpers.c
+++ pset3/music/helpers.c
@@ -2,37 +2,81 @@
 
 #include <cs50.h>
 #include <string.h>
-#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
 
 #include "helpers.h"
 
 // Converts a fraction formatted as X/Y to eighths
 int duration(string fraction)
 {
-    int beat;
+    int denom = ((strlen(fraction)) - 1) - '0';
+    int beat = 8/denom;
 
-    for (int i = 0, length = strlen(fraction); i < length; i++)
-    {
-        if (fraction[i] == 49)
-        {
-            beat = 1;
-        }
-        else if (fraction[i] == 50)
-        {
-            beat = 2;
-        }
-        else
-        {
-            beat = 4;
-        }
-    }
     return beat;
 }
 
 //Calculates frequency (in Hz) of a note
 int frequency(string note)
 {
-    // TODO
+    int freq;
+    int semitone;
+    int octave = ((strlen(note)) - 1) - '0';
+
+    switch (note[0])
+    {
+        case 'A':
+        semitone = 0.0;
+        break;
+
+        case 'B':
+        semitone = 2.0;
+        break;
+
+        case 'C':
+        semitone = -9.0;
+        break;
+
+        case 'D':
+        semitone = -7.0;
+        break;
+
+        case 'E':
+        semitone = -5.0;
+        break;
+
+        case 'F':
+        semitone = -4.0;
+        break;
+
+        case 'G':
+        semitone = -2.0;
+        break;
+
+        default:
+        return 0;
+    }
+
+        if (note[1] == 35)
+        {
+            semitone = semitone + 1.0;
+        }
+        else if (note[1] == 98)
+        {
+            semitone = semitone - 1.0;
+        }
+
+    if (octave <= 8 && octave >= 0)
+        {
+            semitone = semitone + ((octave - 4.0) * 12.0);
+        }
+    // if (octave > 8 || octave < 0)
+    // {
+    //     return 0;
+    // }
+
+    freq = round(pow(2.0, (semitone/12.0)) * 440.00);
+    return freq;
 }
 
 // Determines whether a string represents a rest
