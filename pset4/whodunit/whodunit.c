628943722b8a9451b6986d87c07c246fc9985298
Rose Parker <parker.rose@ymail.com>
2018-12-13 03:28:20 +0000

    program accepts exactly two commnd line arguments, no less or no more else it reminds the user of correct usage

---
+++ pset4/whodunit/whodunit.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(int argv, string argc[])
+{
+    if (argv != 3)
+    {
+        fprintf(stderr, "Usage: ./whodunit infile outfile\n");
+    }
+    else
+    {
+        return 1;
+    }
+}
