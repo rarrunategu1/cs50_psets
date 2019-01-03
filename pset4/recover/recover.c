c554ab1b7205df2529de08b1e244a26d444eb5be
Rose Parker <parker.rose@ymail.com>
2019-01-03 03:41:50 +0000

    able to create a file but it is of invalid format.  Still in process of finishing the code in the while loop

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -1,6 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
-#include <stdlib.h>
+
+#define FAT_BLOCK_SIZE 512
 
 int main (int argc, char* argv[])
 {
@@ -12,10 +13,45 @@ int main (int argc, char* argv[])
 
     char* forensicPic = argv[1];
 
-    FILE *image = fopen(forensicPic, "r");
-    if (image == NULL)
+    FILE *raw_file = fopen(forensicPic, "r");
+    if (raw_file == NULL)
     {
         fprintf(stderr, "Could not open %s for reading.\n", forensicPic);
         return 2;
     }
+
+    unsigned char *buffer = malloc(FAT_BLOCK_SIZE);
+    if (buffer == NULL)
+    {
+        fprintf(stderr, "Memory is not sufficient");
+    }
+
+
+    char recoveredJpegs[8];
+
+    //reads blocks from the images
+    while (fread(buffer, FAT_BLOCK_SIZE, 1, raw_file))
+    {
+        if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+        {
+            //saves the files one at a time
+            sprintf(recoveredJpegs, "%03i.jpg", 2);
+
+        }
+
+
+    //opens file to with writin permissions
+    FILE *img = fopen(recoveredJpegs,"w");
+
+        //if block doesn't indicate a jpeg header keep reading
+        //if the block is a jpeg header then you know it's a jpeg
+        //once you reach another jpeg header you reached the end of a jpeg so you can close it and start a new one which is the beginning of a jpeg
+
+    //writes to file
+    //fwrite(data, size, number, recoveredJpegs);
+
+
+}
+
+
 }
